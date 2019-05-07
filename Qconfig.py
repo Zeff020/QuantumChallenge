d90dc59137c2373225a39e116901d12852b6bbfe
Zeff020 <zefwolffs@gmail.com>
2019-05-07 11:45:41 +0200

    Upload before final

---
+++ Qconfig.py
@@ -0,0 +1,5 @@
+APItoken = '97191735d60a51a27bfdf3f218f08ac72ddec1328602b55f286f1d06ba266a14838e800cf491aed5fa363c061bfc1b377075255743bbf58c7707875152c523de'
+
+config = {
+    'url': 'https://quantumexperience.ng.bluemix.net/api'
+}
